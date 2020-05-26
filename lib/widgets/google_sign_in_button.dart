06808ea26b5b18cc60f9680e321a6ebc1dfff524
Elijah Luckey <luckeyelijah112@gmail.com>
2020-05-26 12:33:37 -0400

    Google Sign in button widget

---
+++ lib/widgets/google_sign_in_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignInButton extends StatelessWidget {
+  GoogleSignInButton({this.onPressed});
+
+  // custom onPressed funtion
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    Image _buildLogo() {
+      return Image.asset(
+        'image/google-logo.png',
+        height: 18.0,
+        width: 18.0,
+      );
+    }
+
+    Opacity _buildText() {
+      return Opacity(
+        opacity: 0.54,
+        child: Text(
+          'Sign in with Google',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            color: Colors.black,
+          ),
+        ),
+      );
+    }
+
+    return MaterialButton(
+      height: 40.0,
+      onPressed: this.onPressed,
+      color: Colors.white,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          _buildLogo(),
+          SizedBox(width: 12.0),
+          _buildText(),
+        ],
+      ),
+    );
+  }
+}
